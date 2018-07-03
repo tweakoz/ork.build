6877562262d45fec5398d3a78bffea228c446ec1
TweakoZ <michael@tweakoz.com>
2018-07-02 21:29:03 -0700

    add minetest_game and mods

--- deps/minetest.py
+++ deps/minetest.py
@@ -6,8 +6,8 @@
 # see http://www.gnu.org/licenses/gpl-2.0.html
 ###############################################################################
 
-VERSION = "0.4"
-HASH = "e754d6ca543d943ab1364e55d48b2840"
+VERSION = "03edcafdda550e55e29bf48a682097028ae01306"
+HASH = "1453e79e663ea3bd8b528ef20a17109b"
 
 import os, tarfile
 from yarl import URL
@@ -34,7 +34,7 @@ class minetest(dep.Provider):
     self.build_dest = build_dest
     self.manifest = path.manifests()/"minetest"
     self.OK = self.manifest.exists()
-    self.fname = "minetest-stable-%s.zip"%VERSION
+    self.fname = "minetest-%s.zip" % VERSION
     if False==self.OK:
       self.download_and_extract()
       self.OK = self.build()
@@ -43,7 +43,7 @@ class minetest(dep.Provider):
 
   def download_and_extract(self): #############################################
 
-    url = URL("https://github.com/minetest/minetest/archive/stable-%s.zip"%VERSION)
+    url = URL("https://github.com/minetest/minetest/archive/%s.zip"%VERSION)
 
     self.arcpath = dep.downloadAndExtract([url],
                                           self.fname,
@@ -52,9 +52,19 @@ class minetest(dep.Provider):
                                           self.build_dest)
 
 
+    source_dir = self.build_dest/("minetest-%s"%VERSION)
+    os.chdir(str(source_dir/"games"))
+    Command(["git","clone","https://github.com/minetest/minetest_game"]).exec()
+
+    os.chdir(str(source_dir/"games"/"minetest_game"/"mods"))
+
+    # install mods
+    for item in "technic mesecons pipeworks moreores digtron lightning".split():
+      Command(["git","clone","https://github.com/minetest-mods/%s"%item]).exec()
+
   def build(self): ############################################################
 
-    source_dir = self.build_dest/("minetest-stable-%s"%VERSION)
+    source_dir = self.build_dest/("minetest-%s"%VERSION)
     build_temp = source_dir/".build"
     print(build_temp)
     if build_temp.exists():
@@ -67,6 +77,8 @@ class minetest(dep.Provider):
     if host.IsOsx:
       cmakeEnv["CMAKE_MACOSX_RPATH"]=1
       cmakeEnv["CMAKE_INSTALL_RPATH"]=path.prefix()/"lib"
+      cmakeEnv["GETTEXT_INCLUDE_DIR"]="/usr/local/opt/gettext/include"
+      cmakeEnv["GETTEXT_LIBRARY"]="/usr/local/opt/gettext/lib"
 
     CMakeContext(root=source_dir,env=cmakeEnv).exec()
     return 0==Command(["make","-j",host.NumCores,"install"]).exec()
