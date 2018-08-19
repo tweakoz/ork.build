f27e9eaab74b6586e9b804f4e4ed62985907760b
TweakoZ <michael@tweakoz.com>
2018-08-19 16:21:38 -0700

    add deps to system init

--- scripts/_systeminit_bionic.sh
+++ scripts/_systeminit_bionic.sh
@@ -8,7 +8,9 @@ apt install libc++abi-dev libc++-dev libssl-dev g++
 apt install mesa-common-dev freeglut3-dev 
 apt install libxinerama-dev libxcursor-dev libxrandr-dev x11proto-randr-dev
 apt install gcc-multilib g++-multilib 
-apt install python3 python3-pip git vim wget scons ninja-build 
-
+apt install python3 python3-pip git vim wget scons ninja-build libelf-dev
+apt install libmpc-dev libgmp-dev libmpfr-dev
+apt install libncurses5-dev libreadline-dev
 pip3 install virtualenv
+pip3 install yarl
 python3 -m pip install python-dateutil
