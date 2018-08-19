c95a00ef8d61fa4a50b0d93cee7af0eb41101785
TweakoZ <michael@tweakoz.com>
2018-08-19 14:54:33 -0700

    add system init script for ubuntu bionic

---
+++ scripts/_systeminit_bionic.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+
+apt update
+apt install autoconf automake bison libbison-dev clang clang-4.0 clang-5.0 cmake curl flex 
+apt install make p7zip libsqlite3-dev libusb-dev libzstd-dev unzip g++-6 libgconf-2-4
+apt install v4l-utils net-tools ninja-build
+apt install libc++abi-dev libc++-dev libssl-dev g++
+apt install mesa-common-dev freeglut3-dev 
+apt install libxinerama-dev libxcursor-dev libxrandr-dev x11proto-randr-dev
+apt install gcc-multilib g++-multilib 
+apt install python3 git vim  wget scons  ninja-build 
