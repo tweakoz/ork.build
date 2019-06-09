1629e418da29e0fec65064fe24806c6fca53c27c
Michael T. Mayers <michael@tweakoz.com>
2019-06-09 13:52:38 -0700

    builder for litex zephyr example

---
+++ examples/litex_zephyrtest/build.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+from ork import path, pathtools, utils
+import os
+
+build_dir = path.builds()/"litex_zephyrtest"
+this_dir = os.path.dirname(os.path.realpath(__file__))
+
+pathtools.mkdir(build_dir,clean=True)
+pathtools.chdir(build_dir)
+os.system("cmake %s"%str(this_dir))
+os.system("make -j %d"%utils.num_cores)
