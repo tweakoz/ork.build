df6069998562d4e5694b968f909dcd56d29e1c37
Michael T. Mayers <michael@tweakoz.com>
2019-06-30 00:44:17 -0700

    add config.py

---
+++ scripts/ork/config.py
@@ -0,0 +1,19 @@
+# persistent configuration
+
+import json
+
+def load(configpath,defaults):
+    inpstr = ""
+    overlay_dict = dict()
+    ########################
+    try:
+        with open(str(configpath),"r") as f:
+            inpstr = f.read()
+            overlay_dict = json.loads(inpstr)
+    except:
+        pass
+    ########################
+    config_dict = dict(defaults, **overlay_dict)
+    with open(str(configpath),"w") as f:
+        f.write(json.dumps(config_dict, sort_keys=True, indent=4 ))
+    return config_dict
