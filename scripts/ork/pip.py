5896290a4402efd6ef78de6604362eee43c7175d
Michael T. Mayers <michael@tweakoz.com>
2019-12-12 22:00:34 -0800

    pip naming is confusing

--- scripts/ork/pip.py
+++ scripts/ork/pip.py
@@ -7,6 +7,12 @@ def install(name_or_list):
   python_prefix = Command(python_prefix_cmd).capture().replace("\n","")
   python_prefix = Path(python_prefix)
   pip_executable = python_prefix/"bin"/"pip"
+  pip_executable = python_prefix/"bin"/"pip3"
+  ## try pip3 first  
+  if False==pip_executable.exists():
+      ## if there is no pip3, hopefully the pip pointed to by the
+      ##  above python3-config is a python3 pip
+      pip_executable = python_prefix/"bin"/"pip"
   rval = 0
   if(isinstance(name_or_list,str)):
     r = Command([pip_executable,"install",name_or_list]).exec()
