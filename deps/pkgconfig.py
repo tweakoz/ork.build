6ffb3ca0256fd84fd9191150a398982219c6a7e0
Michael T. Mayers <michael@tweakoz.com>
2019-12-23 02:28:50 -0800

    fix pkgconfig build issue by updating it. cmake infinite recursion temp fix

--- deps/pkgconfig.py
+++ deps/pkgconfig.py
@@ -4,12 +4,15 @@ from ork.deco import Deco
 from ork.wget import wget
 from ork.command import Command
 
-VER = "0.28"
-HASH = "aa3c86e67551adc3ac865160e34a2a0d"
+VER = "0.29.2"
+HASH = "f6e931e319531b736fadc017f470e68a"
 
 class pkgconfig(dep.Provider):
 
   def __init__(self,options=None): ############################################
+
+    parclass = super(pkgconfig,self)
+    parclass.__init__(options=options)
 
     self.manifest = path.manifests()/"yarl"
     self.extract_dir = path.builds()/"pkgconfig"
@@ -17,14 +20,9 @@ class pkgconfig(dep.Provider):
     self.build_dir = self.source_dir/".build"
     self.url = "http://pkgconfig.freedesktop.org/releases/pkg-config-%s.tar.gz" % VER
 
-    if "force" in options and options["force"]==True:
-      pass
+    self.OK = self.manifest.exists()
 
-    elif self.manifest.exists():
-        self.OK = True
-        return
-
-  def provide(self): ##########################################################
+  def build(self): ##########################################################
 
     self.arcpath = dep.downloadAndExtract([self.url],
                                           "pkg-config-%s" % VER,
@@ -43,7 +41,18 @@ class pkgconfig(dep.Provider):
 
     #assert(False)
 
-    make.exec( "install" )
-    self.manifest.touch()
-    self.OK = True
+    if host.IsLinux:
+        f2r = path.stage()/"bin"/"x86_64-unknown-linux-gnu-pkg-config"
+        os.system( "rm -f %s" % f2r)
+
+    self.OK = (make.exec( "install" )==0)
+    if self.OK:
+        self.manifest.touch()
+    return self.OK
+
+  def provide(self): ##########################################################
+    if self.should_build():
+      self.OK = self.build()
+      if self.OK:
+        self.manifest.touch()
     return self.OK
