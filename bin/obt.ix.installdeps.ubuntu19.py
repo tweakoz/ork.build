ebb45a6bd77f9b8e290f6aa245af29acca9fe1c5
Michael T. Mayers <michael@tweakoz.com>
2020-02-13 00:27:29 -0800

    ..

--- bin/obt.ix.installdeps.ubuntu19.py
+++ bin/obt.ix.installdeps.ubuntu19.py
@@ -26,6 +26,7 @@ deplist += ["libx11-xcb-dev"]
 deplist += ["libxcb-glx0-dev"]
 deplist += ["libavformat-dev"]
 deplist += ["libavcodec-dev"]
+deplist += ["libssl-dev"]
 deplist += ["wget","git","vim"]
 
 for item in deplist:
