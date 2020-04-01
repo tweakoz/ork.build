3040c5c5237431f2c5defa665b8159155cc6c854
Michael T. Mayers <michael@tweakoz.com>
2020-03-31 17:09:50 -0700

    add easy profiler

---
+++ deps/easyprof.py
@@ -0,0 +1,31 @@
+###############################################################################
+# Orkid Build System
+# Copyright 2010-2018, Michael T. Mayers
+# email: michael@tweakoz.com
+# The Orkid Build System is published under the GPL 2.0 license
+# see http://www.gnu.org/licenses/gpl-2.0.html
+###############################################################################
+
+VERSION ="v2.1.0"
+
+import os, tarfile
+from ork import dep, host, path, git, cmake, make
+from ork.deco import Deco
+from ork.wget import wget
+from ork.command import Command
+from ork.cmake import context
+
+deco = Deco()
+
+###############################################################################
+
+class easyprof(dep.StdProvider):
+  #############################################
+  def __init__(self):
+    name = "easyprof"
+    super().__init__(name)
+    self._fetcher = dep.GitFetcher(name)
+    self._fetcher._git_url = "https://github.com/yse/easy_profiler"
+    self._fetcher._revision = VERSION
+    self._builder = dep.CMakeBuilder(name)
+  #############################################
