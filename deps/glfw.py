6c24e7242a54443b7afa381ac41cbe8fc1180eab
Michael T. Mayers <michael@tweakoz.com>
2020-04-22 04:04:47 -0700

    ..

--- deps/glfw.py
+++ deps/glfw.py
@@ -6,7 +6,7 @@
 # see http://www.gnu.org/licenses/gpl-2.0.html
 ###############################################################################
 
-from ork import dep
+from ork import dep, host, command, path
 
 ###############################################################################
 
@@ -18,5 +18,24 @@ class glfw(dep.StdProvider):
     self._fetcher = dep.GitFetcher(name)
     self._fetcher._git_url = "https://github.com/glfw/glfw"
     self._fetcher._revision = "master"
-
-    self._builder = dep.CMakeBuilder(name)
+    ###########################################
+    ## GLFW installs with wrong rpath install name
+    ##  on mac, so we fix it up here..
+    ###########################################
+    class FixOsx(dep.CMakeBuilder):
+      def __init__(self,name):
+        super().__init__(name)
+      def install(self,blddir):
+        success = super().install(blddir)
+        if success:
+          dylibname = "libglfw.3.dylib"
+          retc = command.run(["install_name_tool","-id",
+                              "@rpath/"+dylibname,path.libs()/dylibname])
+          success = retc==0
+        return success
+    ###########################################
+    builder_class = dep.switch(linux=dep.CMakeBuilder,
+                               macos=FixOsx)
+    self._builder = builder_class(name)
+    #self._builder.setCmVar("GLFW_VULKAN_STATIC","TRUE")
+    ###########################################
