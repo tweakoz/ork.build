a24e54facac62f0d38719c7e784bc5b75bae899f
Michael T. Mayers <michael@tweakoz.com>
2020-05-05 12:30:44 -0700

    fixes for doxygen

--- scripts/ork/template.py
+++ scripts/ork/template.py
@@ -2,6 +2,9 @@ from string import Template
 import ork.deco
 deco = ork.deco.Deco()
 
+class OrkTemplate(Template):
+  delimiter = "$$$" # override delimiter because sometimes we do files with $ in them
+
 def template_file(inppath, replacements, outpath=None):
 
     if outpath==None:
@@ -13,6 +16,9 @@ def template_file(inppath, replacements, outpath=None):
     with open(str(inppath),"r") as f:
         inpstr = f.read()
     with open(str(outpath),"w") as outfile:
-        t = Template(inpstr)
+        t = OrkTemplate(inpstr)
+        print(inpstr)
+        print(t)
+        print(replacements)
         outstr = t.substitute(replacements)
         outfile.write(outstr)
