715dc5c8ea55986781a139d9f736a9028d767ea7
Michael T. Mayers <michael@tweakoz.com>
2022-07-04 14:10:00 -0600

    ..

--- modules/docker/amigadev/test1/main.cpp
+++ modules/docker/amigadev/test1/main.cpp
@@ -2,15 +2,30 @@
 #include <proto/dos.h>
 #include <string>
 
-
+using uint16_t = unsigned short;
+using reg16_t = volatile uint16_t*;
 
 int main(int argc, char **argv )
 {
+
+
+    auto COLOR0 = reg16_t(0xDFF180);
+
+    auto setcolor = [](uint16_t color, uint16_t val){
+        auto reg_addr = reg16_t(0xDFF180+(color<<1));
+        (*reg_addr) = val;
+    };
+
+
+    for( int i=0; i<32; i++ )
+        setcolor(i,0);
+
     auto SysBase = *((Library **)4UL);
     auto DOSBase = OpenLibrary("dos.library", 0);
 
-    auto writestr = [DOSBase](const std::string s){
-        auto outputstr = Output();
+    auto outputstr = Output();
+    
+    auto writestr = [DOSBase,outputstr](const std::string s){
         Write(outputstr, (void*) s.c_str(), s.length());
     };
 
---
+++ modules/docker/amigadev/test1/test_fsuae.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3 
+
+from yarl import URL
+from ork import command, path, pathtools, wget
+
+
+builddir = path.builds()/"amigadev-test1"
+pathtools.chdir(builddir)
+
+archive = URL("https://archive.org/download")
+
+# fetch kickstarts
+zipname = "Amiga-Kickstart-ROMs-Commodore-A500-A600-A1000-A3000-A4000-A1200-A2000.zip"
+url = archive/"amiga-kickstart-roms-commodore-a-500-a-600-a-1000-a-3000-a-4000-a-1200-a-2000"/zipname
+wget.wget( urls=[url],
+           output_name=path.downloads()/zipname,
+           md5val="2765730b20470e7001d656d1f49b721d")
+command.run(["unzip","-o",path.downloads()/zipname])
+
+# fetch DOS disk
+zipname = "AmigaDOS CLI rev 27.5 (1985)(Commodore)[m].zip"
+wget.wget( urls=[archive/"CommodoreAmigaApplicationsADF"/zipname],
+           output_name=path.downloads()/zipname,
+           md5val="f5b9e49849dc04ddb35b1d6de3155fa2")
+command.run(["unzip","-o",path.downloads()/zipname])
+
+command.run(["fs-uae",
+             "--hard_drive_0=.",
+             "--floppy-drive-0=AmigaDOS CLI rev 27.5 (1985)(Commodore)[m].adf",
+             "--amiga-model=a4000 ",
+             "--kickstart_file=./Kickstart-v3.1-rev40.70-1994-Commodore-A4000.rom",
+             "--floppy_drive_volume=0"])
