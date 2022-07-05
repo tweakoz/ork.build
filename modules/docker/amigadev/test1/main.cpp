d2e9610a45516fdfa80409b2607d805dbfd99f6d
Michael T. Mayers <michael@tweakoz.com>
2022-07-04 20:48:40 -0600

    ..

--- modules/docker/amigadev/test1/main.cpp
+++ modules/docker/amigadev/test1/main.cpp
@@ -3,41 +3,37 @@
 #include <string>
 
 using uint16_t = unsigned short;
-using reg16_t = volatile uint16_t*;
+using reg16_t  = volatile uint16_t*;
 
-int main(int argc, char **argv )
-{
+int main(int argc, char** argv) {
 
+  auto COLOR0 = reg16_t(0xDFF180);
 
-    auto COLOR0 = reg16_t(0xDFF180);
+  auto setcolor = [](uint16_t color, uint16_t val) {
+    auto reg_addr = reg16_t(0xDFF180 + (color << 1));
+    (*reg_addr)   = val;
+  };
 
-    auto setcolor = [](uint16_t color, uint16_t val){
-        auto reg_addr = reg16_t(0xDFF180+(color<<1));
-        (*reg_addr) = val;
-    };
+  auto SysBase = *((Library**)4UL);
+  auto DOSBase = OpenLibrary("dos.library", 0);
 
+  auto outputstr = Output();
 
-    for( int i=0; i<32; i++ )
-        setcolor(i,0);
+  auto writestr = [DOSBase, outputstr](const std::string s) { Write(outputstr, (void*)s.c_str(), s.length()); };
 
-    auto SysBase = *((Library **)4UL);
-    auto DOSBase = OpenLibrary("dos.library", 0);
+  printf("DOSBase<%p>\n", (void*)DOSBase);
 
-    auto outputstr = Output();
-    
-    auto writestr = [DOSBase,outputstr](const std::string s){
-        Write(outputstr, (void*) s.c_str(), s.length());
-    };
+  if (DOSBase) {
+    writestr("Hello World\n");
 
-    printf("DOSBase<%p>\n", (void*) DOSBase );
-
-    if (DOSBase) {
-        writestr("Hello World\n");
-        writestr("wtf1\n");
-        writestr("wtf2\n");
-        writestr("wtf3\n");
-        CloseLibrary(DOSBase);
+    while (true) {
+      auto VHPOSR  = reg16_t(0xDFF006);
+      uint16_t val = *VHPOSR;
+      setcolor(0, val);
     }
 
-    return(0);
+    CloseLibrary(DOSBase);
+  }
+
+  return (0);
 }
