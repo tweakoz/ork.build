cb407846a0a396ad5ecd7b80b6f780a745447cb4
Michael Mayers <michael@tweakoz.com>
2023-03-25 17:35:19 -0600

    premake, xatlas

---
+++ modules/dep/premake.py
@@ -0,0 +1,51 @@
+###############################################################################
+# Orkid Build System
+# Copyright 2010-2018, Michael T. Mayers
+# email: michael@tweakoz.com
+# The Orkid Build System is published under the GPL 2.0 license
+# see http://www.gnu.org/licenses/gpl-2.0.html
+###############################################################################
+
+VERSION = "v5.0.0-beta2"
+
+from ork import dep, host, path
+from ork.command import Command as cmd
+
+###############################################################################
+
+class premake(dep.StdProvider):
+  name = "premake"
+  def __init__(self): ############################################
+    super().__init__(premake.name)
+    self.declareDep("cmake")
+    self._builder = self.createBuilder(dep.CustomBuilder)
+    self._builder._incrbuildcommands = [
+      cmd(["make","-f","Bootstrap.mak","linux"])
+    ]
+    self._builder._installcommands += {
+      cmd(["install","bin/release/premake5",path.bin()/"premake5"])
+    }
+    #if host.IsDarwin:
+    #  self._builder.setCmVar("WITH_TLS","OFF")
+  ########################################################################
+  def __str__(self): 
+    return "premake (github-%s)" % VERSION
+  ########################################################################
+  @property
+  def _fetcher(self):
+    return dep.GithubFetcher(name=premake.name,
+                             repospec="premake/premake-core",
+                             revision=VERSION,
+                             recursive=False)
+  ########################################################################
+  #def linkenv(self):
+  #  LIBS = ["premake"]
+  #  return {
+  #      "LIBS": LIBS,
+  #      "LFLAGS": ["-l%s"%item for item in LIBS]
+  #  }
+  ########################################################################
+  def areRequiredSourceFilesPresent(self):
+    return (self.source_root/"doxyfile").exists()
+  def areRequiredBinaryFilesPresent(self):
+    return (path.includes()/"premake.h").exists()
