340f5a82218e897cf6789a9f4e91406567aa7181
Michael Mayers <michael@tweakoz.com>
2023-07-15 13:06:58 -0600

    ..

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 import glob, os
 import platform
 
-version = "0.0.45"
+version = "0.0.46"
 
 ###############################################################################
 
