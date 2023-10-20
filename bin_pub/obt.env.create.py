0ff23cc4f9f4d4a8b83c13971513f8d55d129166
Michael Mayers <michael@tweakoz.com>
2023-10-19 20:04:02 -0600

    ..

--- bin_pub/obt.env.create.py
+++ bin_pub/obt.env.create.py
@@ -90,7 +90,7 @@ print(os.environ)
 print(os.environ["OBT_MODULES_PATH"])
 os.system("ls %s" % os.environ["OBT_MODULES_PATH"])
 
-os.system("ls -l /home/workerub22/.out-worker/orkdotbuild-ix-ub2204/develop/repo")
+os.system("find /home/workerub22/.out-worker/orkdotbuild-ix-ub2204/develop/repo/modules")
 MANDATORY_DEPS = ["cmake","python","pydefaults"]
 
 import obt.dep
