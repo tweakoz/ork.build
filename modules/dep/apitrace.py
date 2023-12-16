ea5660bb9d503f00d09b6e84c98aa5d6ab2ed7f6
Michael T. Mayers <michael@tweakoz.com>
2023-12-15 17:45:58 -0700

    fix apitrace (WIP)

--- modules/dep/apitrace.py
+++ modules/dep/apitrace.py
@@ -1,67 +1,39 @@
 ###############################################################################
 # Orkid Build System
-# Copyright 2010-2018, Michael T. Mayers
+# Copyright 2010-2020, Michael T. Mayers
 # email: michael@tweakoz.com
 # The Orkid Build System is published under the GPL 2.0 license
 # see http://www.gnu.org/licenses/gpl-2.0.html
 ###############################################################################
 
-VERSION ="master"
-
-import os, tarfile
-from obt import dep, host, path, git, cmake, make
-from obt.deco import Deco
-from obt.wget import wget
-from obt.command import Command
-from obt.cmake import context
-
-deco = Deco()
+from obt import dep, path
 
 ###############################################################################
 
-class apitrace(dep.Provider):
+class apitrace(dep.StdProvider):
+  name = "apitrace"
+  def __init__(self):
+    super().__init__(apitrace.name)
+    self.declareDep("cmake")
+    self._builder = self.createBuilder(dep.CMakeBuilder)
+  ########################################################################
+  @property
+  def github_repo(self):
+    return "apitrace/apitrace"
+  ########################################################################
+  @property
+  def revision(self):
+    return "11.1"
+  ########################################################################
+  @property
+  def _fetcher(self):
+    return dep.GithubFetcher(name=apitrace.name,
+                             repospec=self.github_repo,
+                             revision=self.revision,
+                             recursive=True)
 
-  def __init__(self,miscoptions=None): ############################################
-
-    super().__init__("apitrace")
-
-    self.source_root = path.builds()/"apitrace"
-    self.build_dest = path.builds()/"apitrace"/".build"
-    self.manifest = path.manifests()/"apitrace"
-
-    self.OK = self.manifest.exists()
-    self._archlist = ["x86_64"]
-
-  def __str__(self): ##########################################################
-
-    return "ApiTrace (github-%s)" % VERSION
-
-  def build(self): ##########################################################
-
-    git.Clone("https://github.com/apitrace/apitrace",self.source_root,VERSION)
-
-    os.system("rm -rf %s"%self.build_dest)
-    os.mkdir(self.build_dest)
-    os.chdir(self.build_dest)
-    cmake_ctx = cmake.context("..",env={
-      "ENABLE_GUI":"TRUE"
-    })
-    cmake_ctx.exec()
-    return (make.exec("install")==0)
-
-  def linkenv(self): ##########################################################
-    return {
-        "LIBS": ["apitrace"]
-    }
-
-  def provide(self): ##########################################################
-
-    if self.should_build:
-
-      self.OK = self.build()
-      if self.OK:
-        self.manifest.touch()
-
-    print(self.OK)
-
-    return self.OK
+  ########################################################################
+  def areRequiredSourceFilesPresent(self):
+    return (self.source_root/"CMakeLists.txt").exists()
+  def areRequiredBinaryFilesPresent(self):
+    return (path.bin()/"apitrace").exists()
