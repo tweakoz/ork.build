d65758925a45b627169ca4d56f8a3fa2d5828f6e
Michael T. Mayers <michael@tweakoz.com>
2024-06-26 03:48:14 -0600

    ..

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.10',
     install_requires=[
         'yarl',
         'toposort',
