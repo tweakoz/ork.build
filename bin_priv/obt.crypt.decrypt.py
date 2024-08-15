dd4b612d488ab789abf44ca250ff237d2cc29798
Michael T. Mayers <michael@tweakoz.com>
2024-08-15 15:30:19 -0600

    ..

--- bin_priv/obt.crypt.decrypt.py
+++ bin_priv/obt.crypt.decrypt.py
@@ -4,20 +4,34 @@
 
 from obt import path, wget, crypt 
 import sys, os, random, numpy, argparse
+import base64
 
 parser = argparse.ArgumentParser(description='assetpak tester')
-parser.add_argument("-o", '--output', type=str, help='output file', required=True)
-parser.add_argument("-i", '--input', type=str, help='input file', required=True)
-parser.add_argument("-s", '--string', type=str, help='string', required=True)
+parser.add_argument("-o", '--output', type=str, help='output file', required=False)
+parser.add_argument("-i", '--input', type=str, help='input file', required=False)
+parser.add_argument("-s", '--string', type=str, help='string', required=False)
 parser.add_argument("-p", '--passphrase', type=str, help='passphrase', required=True)
 args = vars(parser.parse_args())
 
 output = args['output']
 input = args['input']
 passphrase = args['passphrase']
+input_string = args.get('string')
 
-p_output = path.Path(output)
-if p_output.exists():
-  p_output.unlink()
-  
-crypt.decrypt_file(input, output, passphrase)
+if input_string:
+  assert(input == None)
+  assert(output == None)
+  #strip quotes surrounding the string if they exist
+  if input_string[0] == '"' and input_string[-1] == '"':
+    input_string = input_string[1:-1]
+  # Decode the base64 encoded string
+  input_string = base64.urlsafe_b64decode(input_string)
+  # Encrypt the string
+  decrypted_string = crypt.decrypt_string(input_string, passphrase)
+  print(decrypted_string)
+  # Write the encoded string to the output file
+else:
+  p_output = path.Path(output)
+  if p_output.exists():
+    p_output.unlink()
+  crypt.decrypt_file(input, output, passphrase)
--- bin_priv/obt.crypt.encrypt.py
+++ bin_priv/obt.crypt.encrypt.py
@@ -7,7 +7,7 @@ import sys, os, random, numpy, argparse
 import base64
 
 parser = argparse.ArgumentParser(description='assetpak generator')
-parser.add_argument("-o", '--output', type=str, help='output file', required=True)
+parser.add_argument("-o", '--output', type=str, help='output file', required=False)
 parser.add_argument("-i", '--input', type=str, help='input file', required=False)
 parser.add_argument("-s", '--string', type=str, help='string', required=False)
 parser.add_argument("-p", '--passphrase', type=str, help='passphrase', required=True)
@@ -19,18 +19,15 @@ input_string = args.get('string')
 passphrase = args['passphrase']
 
 if input_string:
-    # Encrypt the string
-    encrypted_data = crypt.encrypt_string(input_string, passphrase)
-    # Encode the encrypted data to an alphanumeric string using base64
-    encoded_string = base64.urlsafe_b64encode(encrypted_data).decode('utf-8')
-    
-    # Write the encoded string to the output file
-    with open(output, 'w') as f:
-        f.write(encoded_string)
-else:
-    if not input:
-        print("Error: You must provide either an input file or a string.")
-        sys.exit(1)
+  #strip quotes surrounding the string if they exist
+  if input_string[0] == '"' and input_string[-1] == '"':
+    input_string = input_string[1:-1]
 
-    # Encrypt the file
-    crypt.encrypt_file(input, output, passphrase)
+  # Encrypt the string
+  encrypted_data = crypt.encrypt_string(input_string, passphrase)
+  # Encode the encrypted data to an alphanumeric string using base64
+  encoded_string = base64.urlsafe_b64encode(encrypted_data).decode('utf-8')
+  print(encoded_string)
+  # Write the encoded string to the output file
+else:
+  crypt.encrypt_file(input, output, passphrase)
