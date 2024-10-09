96a0e0e5cd760ff7dc724f3bffd1871b509a9f05
Michael Mayers <tweakoz@users.noreply.github.com>
2024-10-08 18:20:03 -0600

    Update _obt.ix.install.docker_x86_64.py
    add docker buildx options (WIP)

--- bin_pub/_obt.ix.install.docker_x86_64.py
+++ bin_pub/_obt.ix.install.docker_x86_64.py
@@ -28,13 +28,12 @@ sys.path.append(str(file_dir))
 
 from _obt_config import configFromCommandLine
 obt_config = configFromCommandLine()
-from obt import path, pathtools, env, command, deco
+from obt import path, pathtools, env, command, deco, wget
 deco = deco.Deco()
 
 ###########################################
 UID = os.getuid()
 USERNAME = getpass.getuser()
-HOMEDIR = os.environ["HOME"]
 
 print(deco.cyan("installing uidmap"))
 command.run(["sudo","apt","install","uidmap", "dbus-user-session"],do_log=True)
@@ -48,7 +47,28 @@ print(deco.cyan("docker installed!"))
 print(deco.cyan("allowing rootless docker to run when logged out"))
 command.run(["sudo","loginctl","enable-linger",USERNAME],do_log=True)
 
+#!/bin/bash
+#
+# https://docs.docker.com/build/buildkit/
+# https://github.com/docker/buildx/releases/
+# https://github.com/docker/buildx
+
+## For Ubuntu 24.04 try: sudo apt install docker-buildx
+## Or run the commands below.
+
+VERSION="v0.17.1"
+HOMEDIR = path.Path(os.environ["HOME"])
+PLUGDIR = HOMEDIR}/".docker"/"cli-plugins"
+pathtools.mkdir(f"{PLUGDIR}",parents=True)
+wget.wget(urls=["https://github.com/docker/buildx/releases/download/$VERSION/buildx-$VERSION.linux-amd64"],
+          output_name = f"{PLUGDIR}/docker-buildx",
+          md5val="0")
+command.run(["chmod", "+x", f"{PLUGDIR}/docker-buildx"])
+
 print(deco.orange("add the following to your .bashrc:"))
 print(deco.yellow("export PATH=%s/bin:$PATH"%HOMEDIR))
 print(deco.yellow("export DOCKER_HOST=unix:///run/user/%d/docker.sock"%UID))
+print(deco.yellow("export DOCKER_BUILDKIT=1"))
+print(deco.yellow("export COMPOSE_DOCKER_CLI_BUILD=1"))
+
 print(deco.yellow("ALSO: see https://docs.docker.com/go/rootless/"))
