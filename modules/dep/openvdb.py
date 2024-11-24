3ea63b7d45ee41065e7b52bc12c5f6dd78fb5b6b
Michael Mayers <tweakoz@users.noreply.github.com>
2024-11-24 10:08:24 -0700

    Update openvdb.py

--- modules/dep/openvdb.py
+++ modules/dep/openvdb.py
@@ -5,7 +5,7 @@
 # The Orkid Build System is published under the GPL 2.0 license
 # see http://www.gnu.org/licenses/gpl-2.0.html
 ###############################################################################
-from obt import dep, path
+from obt import dep, path, pathtools
 ###############################################################################
 class openvdb(dep.StdProvider):
   name = "openvdb"
@@ -15,17 +15,36 @@ class openvdb(dep.StdProvider):
     self.declareDep("blosc")
     self.declareDep("boost")
     self.declareDep("tbb")
+    self.declareDep("nanobind")
     self._builder = self.createBuilder(dep.CMakeBuilder)
     self._builder._cmakeenv = {
-      "BUILD_SHARED_LIBS": "ON"
+      "BUILD_SHARED_LIBS": "ON",
+      "OPENVDB_BUILD_PYTHON_MODULE": "ON",
     }
+
+  def onPostBuild(self):
+    dep_python = dep.instance("python")
+    deconame = dep_python._deconame
+    st_lib = path.stage()/"lib"
+    src_path = st_lib/deconame/"site-packages"
+    dst_path = dep_python.pylib_dir/"site-packages"
+    vcode = dep_python.version_major
+    vcode = vcode.replace(".","")
+    name = f"pyopenvdb.cpython-{vcode}-x86_64-linux-gnu.so"
+    print(vcode)
+    print(name)
+    print(src_path/name)
+    print(dst_path/name)
+    pathtools.copyfile(src_path/name,dst_path/name)
+    #pyopenvdb.cpython-312-x86_64-linux-gnu.so
+    return True
 
   ########################################################################
   @property
   def _fetcher(self):
     fetcher = dep.GithubFetcher(name=openvdb.name,
                                 repospec="AcademySoftwareFoundation/openvdb",
-                                revision="v10.0.0",
+                                revision="v12.0.0",
                                 recursive=False)
     return fetcher
   ########################################################################
