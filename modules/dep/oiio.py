5c1a812f280f833359221815360f3a18d46f76a5
Michael Mayers <tweakoz@users.noreply.github.com>
2024-11-24 15:43:34 -0700

    Update oiio.py

--- modules/dep/oiio.py
+++ modules/dep/oiio.py
@@ -49,7 +49,7 @@ class oiio(dep.StdProvider):
   def _fetcher(self):
     return dep.GithubFetcher(name=oiio.name,
                              repospec="OpenImageIO/oiio",
-                             revision="v2.5.4.0",
+                             revision="v2.5.17.0",
                              recursive=False)
 
   #######################################################################
