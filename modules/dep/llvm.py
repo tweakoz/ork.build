eb214736a72e2877f6332d48b57d79adf7f36aec
Michael Mayers <tweakoz@users.noreply.github.com>
2024-11-25 10:26:40 -0700

    Update llvm.py

--- modules/dep/llvm.py
+++ modules/dep/llvm.py
@@ -16,28 +16,35 @@ class _llvm_from_source(dep.StdProvider):
   name = "llvm"
   def __init__(self,name):
     super().__init__(_llvm_from_source.name)
-    self._archlist = ["x86_64"]
-    self._builder = self.createBuilder(dep.CMakeBuilder)
+    self._archlist = ["x86_64","aarch64"]
     ##########################################
     # llvm cmake file is 1 subdir deeper than usual
     ##########################################
     self.build_src = self.source_root/"llvm"
     self.build_dest = self.source_root/".build"
     ##########################################
+    self._builder = self.createBuilder(
+      dep.CMakeBuilder,
+      src_dir_override=self.build_src)
     self._builder.setCmVars({
         "CMAKE_BUILD_TYPE": "RELEASE",
         "BUILD_SHARED_LIBS": "ON",
         "LLVM_INSTALL_UTILS": "ON",
         "LLVM_ENABLE_DUMP": "ON",
-        "LLVM_ENABLE_PROJECTS": "clang;libcxx;libcxxabi"
+        #"LLVM_ENABLE_PROJECTS": "clang;libcxx;libcxxabi"
     })
+    # arm64-apple-darwin24.1.0
+    if host.IsAARCH64:
+      self._builder.setCmVar("LLVM_TARGETS_TO_BUILD","AArch64")
+    else:
+      self._builder.setCmVar("LLVM_TARGETS_TO_BUILD","X86")
 
   ########################################################################
   @property
   def _fetcher(self):
     fetcher = dep.GithubFetcher(name=_llvm_from_source.name,
                                 repospec="llvm/llvm-project",
-                                revision="llvmorg-12.0.1",
+                                revision="llvmorg-15.0.7",
                                 recursive=False)
     return fetcher
   ########################################################################
@@ -62,8 +69,6 @@ class _llvm_from_homebrew(dep.HomebrewProvider):
 ###############################################################################
 
 BASE = _llvm_from_source
-if host.IsOsx:
-  BASE = _llvm_from_homebrew
 
 ###############################################################################
 
@@ -72,3 +77,7 @@ class llvm(BASE):
     super().__init__("llvm")
   def env_init(self):
     log.marker("registering LLVM SDK")
+~
+~
+~
+~
