d2eca7b38d5048f0c45b8434a3fc3d52f0c22faa
Michael Mayers <tweakoz@users.noreply.github.com>
2024-11-25 10:18:40 -0700

    Update openvdb.py

--- modules/dep/openvdb.py
+++ modules/dep/openvdb.py
@@ -21,6 +21,8 @@ class openvdb(dep.StdProvider):
     self._builder._cmakeenv = {
       "BUILD_SHARED_LIBS": "ON",
       "OPENVDB_BUILD_PYTHON_MODULE": "ON",
+      "OPENVDB_BUILD_AX": "ON",
+      "LLVM_ROOT": path.stage(),
       "CMAKE_FIND_DEBUG_MODE": "OFF",
       "PYTHON_EXECUTABLE": path.pyvenv/"bin"/"python3",
       "PYTHON_LIBRARY": path.pyvenv/"lib"/dep_python.library_file,
@@ -28,10 +30,6 @@ class openvdb(dep.StdProvider):
       "Python_ROOT_DIR": path.pyvenv,
       "VDB_PYTHON_INSTALL_DIRECTORY": path.pyvenv/"lib"/dep_python._deconame/"site-packages",
     }
-    if host.IsDarwin:
-      self._builder._cmakeenv["OPENVDB_BUILD_AX"]="ON"
-      self._builder._cmakeenv["LLVM_ROOT"]="/opt/homebrew/Cellar/llvm@15/15.0.7/"
-      # this will need brew install llvm@15
 
   def onPostInstall(self):
     dep_python = dep.instance("python")
@@ -45,7 +43,7 @@ class openvdb(dep.StdProvider):
     if host.IsDarwin:
       pyext_src_path = self.build_dest/"openvdb"/"openvdb"/"python"
       src_name = f"openvdb.cpython-{vcode}-{platform}.so"
-      dst_name = src_name
+      dst_name = src_name      
       pyext_src_path = pyext_src_path/src_name
       pyext_dst_path = pyext_dst_path/dst_name
     else:
@@ -71,8 +69,8 @@ class openvdb(dep.StdProvider):
   @property
   def _fetcher(self):
     fetcher = dep.GithubFetcher(name=openvdb.name,
-                                repospec="AcademySoftwareFoundation/openvdb",
-                                revision="v12.0.0",
+                                repospec="tweakoz/openvdb",
+                                revision="toz-2024-v12-obt",
                                 recursive=False)
     return fetcher
   ########################################################################
