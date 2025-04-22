f8ffd85d86649bb6ce112cb1cb14d9bb6008efcb
Michael T. Mayers <michael@tweakoz.com>
2025-04-22 15:05:05 -0600

    update mac installdeps

--- bin_pub/obt.osx.installdeps.py
+++ bin_pub/obt.osx.installdeps.py
@@ -1,15 +1,16 @@
-#!/usr/bin/env python3
+#!/Users/michael/.venv-apr22/bin/python3
 
 import os
 
-deplist =  ["pkgconfig","cmake","wget","curl","libtiff","libpng", "git-lfs"]
-deplist += ["portaudio","fmt","m4","bison","flex","xz"]
+deplist =  ["pkgconfig","wget","curl","libtiff","libpng", "git-lfs"]
+deplist += ["portaudio","m4","bison","flex","xz"]
 deplist += ["scons","zlib","tbb", "glew","flac","libsndfile"]
 deplist += ["mpfr","openssl","graphviz","doxygen","swig","tcl-tk"]
 deplist += ["pyqt5","qt5", "gcc@13", "xxhash", "autoconf", "automake" ]
 
 depliststr = " ".join(deplist)
+print(depliststr)
 os.system("brew install %s" % depliststr)
-os.system("brew remove boost")
+#os.system("brew remove boost")
 
 os.system("python3 -m pip install --upgrade setuptools")
