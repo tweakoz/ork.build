6c3e1918dcf058f2245890a19bb33809a4cf2529
Michael T. Mayers <michael@tweakoz.com>
2025-07-20 18:23:37 -0600

    add msquic lib

---
+++ modules/dep/msquic.py
@@ -0,0 +1,31 @@
+###############################################################################
+# Orkid Build System
+# Copyright 2010-2020, Michael T. Mayers
+# email: michael@tweakoz.com
+# The Orkid Build System is published under the GPL 2.0 license
+# see http://www.gnu.org/licenses/gpl-2.0.html
+###############################################################################
+
+from obt import dep, path
+
+###############################################################################
+
+class msquic(dep.StdProvider):
+  name = "msquic"
+  def __init__(self):
+    super().__init__(msquic.name)
+    self.declareDep("cmake")
+    self._builder = self.createBuilder(dep.CMakeBuilder)
+    self._debug = True
+    self._fetcher._debug = True
+    self._builder._debug = True
+  @property
+  def _fetcher(self):
+    return dep.GithubFetcher(name=msquic.name,
+                             repospec="microsoft/msquic",
+                             revision="v2.5.0",
+                             recursive=True)
+  def areRequiredSourceFilesPresent(self):
+    return (self.source_root/"CMakeLists.txt").exists()
+  def areRequiredBinaryFilesPresent(self):
+    return (path.includes()/"msquic"/"msquic.h").exists()
