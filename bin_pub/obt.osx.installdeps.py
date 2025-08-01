0c4593ff9cecdb41f2547863c8e18212b1b46e6f
Michael Mayers <tweakoz@users.noreply.github.com>
2025-08-01 14:54:58 -0600

    Update obt.osx.installdeps.py

--- bin_pub/obt.osx.installdeps.py
+++ bin_pub/obt.osx.installdeps.py
@@ -9,7 +9,7 @@ deplist += ["mpfr","openssl","graphviz","doxygen","swig","tcl-tk"]
 deplist += ["pyqt5","qt5", "gcc@13", "xxhash", "autoconf", "automake" ]
 deplist += ["vulkan-loader", "vulkan-extensionlayer", "vulkan-headers"]
 deplist += ["vulkan-tools", "vulkan-profiles", "vulkan-validationlayers", "vulkan-utility-libraries"]
-deplist += ["pandoc", "doctest","ncurses","curlpp","libtar"]
+deplist += ["pandoc", "doctest","ncurses","curlpp","libtar", "libsodium"]
 
 depliststr = " ".join(deplist)
 print(depliststr)
