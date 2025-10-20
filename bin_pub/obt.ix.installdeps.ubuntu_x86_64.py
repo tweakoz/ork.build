b4b513f983d64df09003ee95eeaaceec793efc08
Michael Mayers <tweakoz@users.noreply.github.com>
2025-10-20 12:43:24 -0600

    Update obt.ix.installdeps.ubuntu_x86_64.py

--- bin_pub/obt.ix.installdeps.ubuntu_x86_64.py
+++ bin_pub/obt.ix.installdeps.ubuntu_x86_64.py
@@ -90,7 +90,7 @@ deplist += ["libsbc-dev","libsdl2-dev","libudev-dev","libva-dev","libv4l-dev","l
 deplist += ["pkg-config","python3-docutils","systemd","mesa-utils","xvfb"]
 deplist += ["meson","ninja-build","libserialport-dev", "libxxhash-dev"]
 deplist += ["libpipewire-0.3-dev", "pipewire", "gstreamer1.0-libav"]
-deplist += ["astap","libnotcurses++-dev","libsodium-dev"]
+deplist += ["astap","libnotcurses++-dev","libsodium-dev","libtar-dev"]
 
 merged = " ".join(deplist)
 os.system("sudo apt -y install %s" % merged)
