17b69c7fe9ff43f7d120c454f631d01e664894c9
Michael T. Mayers <michael@tweakoz.com>
2026-01-08 17:02:30 -0700

    rnnoise fix

--- modules/dep/rnnoise.py
+++ modules/dep/rnnoise.py
@@ -5,7 +5,7 @@
 # The Orkid Build System is published under the GPL 2.0 license
 # see http://www.gnu.org/licenses/gpl-2.0.html
 ###############################################################################
-from obt import dep
+from obt import dep,path
 ###############################################################################
 class rnnoise(dep.StdProvider):
   name = "rnnoise"
