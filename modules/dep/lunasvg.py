d0fdcc37de96ca64fedfd8d338dea0a778c773ce
Michael T. Mayers <michael@tweakoz.com>
2026-03-30 20:34:10 -0600

    add lunasvg

---
+++ modules/dep/lunasvg.py
@@ -0,0 +1,33 @@
+###############################################################################
+# Orkid Build System
+# Copyright 2010-2020, Michael T. Mayers
+# email: michael@tweakoz.com
+# The Orkid Build System is published under the GPL 2.0 license
+# see http://www.gnu.org/licenses/gpl-2.0.html
+###############################################################################
+from obt import dep, path
+###############################################################################
+class lunasvg(dep.StdProvider):
+  name = "lunasvg"
+  def __init__(self):
+    super().__init__(lunasvg.name)
+    self.declareDep("cmake")
+    self._builder = self.createBuilder(dep.CMakeBuilder)
+    self._builder._cmakeenv = {
+      "BUILD_SHARED_LIBS": "ON"
+    }
+
+  ########################################################################
+  @property
+  def _fetcher(self):
+    return dep.GithubFetcher(name=lunasvg.name,
+                             repospec="sammycage/lunasvg",
+                             revision="v3.5.0",
+                             recursive=False)
+  ########################################################################
+
+  def areRequiredSourceFilesPresent(self):
+    return (self.source_root/"CMakeLists.txt").exists()
+
+  def areRequiredBinaryFilesPresent(self):
+    return path.decorate_obt_lib("lunasvg").exists()
