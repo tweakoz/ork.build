6bbe314ad6b709e9aee0ca76a548ecb56dd046f4
Michael T. Mayers <michael@tweakoz.com>
2026-05-20 14:39:20 -0600

    ..

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools.command.install import install
 import os
 import stat
 
-version = "0.0.303dev13"
+version = "0.0.303dev14"
 
 # Read the long description from the README file
 with open("README.md", "r", encoding="utf-8") as fh:
